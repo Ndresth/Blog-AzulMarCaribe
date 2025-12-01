d62b58a366f5c66a0e35d1197dfa9606e7a768d0
Yamith Lobo <yamithadresjulio@gmail.com>
2025-12-01 02:42:24 -0500

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js' // <--- Asegúrate de tener este para el menú móvil
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' 
 import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
